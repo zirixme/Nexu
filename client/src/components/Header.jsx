541baf31f28135e93c99c48c257b5dfd79f6ae95
zirixme <zirix.me@gmail.com>
2025-07-27 11:28:27 +0300

    style: create post popup

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,64 +11,71 @@ import CreateSelected from "../assets/lucide/circle-plus.svg";
 import MessagesSelected from "../assets/lucide/message-square.svg";
 import ProfileSelected from "../assets/lucide/user-round.svg";
 
+import { Create } from "../pages/Create.jsx";
+
+import { useState } from "react";
 import { useLocation } from "react-router";
 
 export const Header = () => {
   const location = useLocation();
+
+  const [toggle, setToggle] = useState(false);
   return (
-    <nav className="fixed bottom-0 md:top-0 w-full md:w-fit px-4 py-2 border md:px-6 md:py-6 border-gray-400 bg-gray-50">
-      <ul className="flex justify-between md:flex-col md:gap-7">
-        <li>
-          <a href="/" className="flex items-center gap-2">
-            {location.pathname === "/" ? (
-              <img src={HomeSelected} alt="home icon" />
-            ) : (
-              <Home />
-            )}
-            <span className="hidden xl:inline">Home</span>
-          </a>
-        </li>
-        <li>
-          <a href="/search" className="flex items-center gap-2">
-            {location.pathname === "/search" ? (
-              <img src={SearchSelected} alt="search icon" />
-            ) : (
-              <UserSearch />
-            )}
-            <span className="hidden xl:inline">Search</span>
-          </a>
-        </li>
-        <li>
-          <a href="/create" className="flex items-center gap-2">
-            {location.pathname === "/create" ? (
-              <img src={CreateSelected} alt="create icon" />
-            ) : (
+    <div>
+      <nav className="fixed bottom-0 md:top-0 w-full md:w-fit px-4 py-2 border md:px-6 md:py-6 border-gray-400 bg-gray-50">
+        <ul className="flex justify-between md:flex-col md:gap-7">
+          <li>
+            <a href="/" className="flex items-center gap-2">
+              {location.pathname === "/" ? (
+                <img src={HomeSelected} alt="home icon" />
+              ) : (
+                <Home />
+              )}
+              <span className="hidden xl:inline">Home</span>
+            </a>
+          </li>
+          <li>
+            <a href="/search" className="flex items-center gap-2">
+              {location.pathname === "/search" ? (
+                <img src={SearchSelected} alt="search icon" />
+              ) : (
+                <UserSearch />
+              )}
+              <span className="hidden xl:inline">Search</span>
+            </a>
+          </li>
+          <li>
+            <button
+              className="flex items-center gap-2 cursor-pointer"
+              onClick={() => setToggle(!toggle)}
+            >
               <CirclePlus />
-            )}
-            <span className="hidden xl:inline">Create</span>
-          </a>
-        </li>
-        <li>
-          <a href="/messages" className="flex items-center gap-2">
-            {location.pathname === "/messages" ? (
-              <img src={MessagesSelected} alt="create icon" />
-            ) : (
-              <MessageSquare />
-            )}
-            <span className="hidden xl:inline">Messages</span>
-          </a>
-        </li>
-        <li>
-          <a href="/profile" className="flex items-center gap-2">
-            {location.pathname === "/profile" ? (
-              <img src={ProfileSelected} alt="create icon" />
-            ) : (
-              <UserRound />
-            )}
-            <span className="hidden xl:inline">Profile</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+              <span className="hidden xl:inline">Create</span>
+            </button>
+          </li>
+          <li>
+            <a href="/messages" className="flex items-center gap-2">
+              {location.pathname === "/messages" ? (
+                <img src={MessagesSelected} alt="create icon" />
+              ) : (
+                <MessageSquare />
+              )}
+              <span className="hidden xl:inline">Messages</span>
+            </a>
+          </li>
+          <li>
+            <a href="/profile" className="flex items-center gap-2">
+              {location.pathname === "/profile" ? (
+                <img src={ProfileSelected} alt="create icon" />
+              ) : (
+                <UserRound />
+              )}
+              <span className="hidden xl:inline">Profile</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {toggle && <Create onClose={() => setToggle(false)} />}
+    </div>
   );
 };
--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,7 +1,38 @@
-export const Create = () => {
+import image from "../assets/lucide/image.svg";
+import clapperboard from "../assets/lucide/clapperboard.svg";
+
+import { XIcon } from "lucide-react";
+
+export const Create = ({ onClose }) => {
   return (
-    <div>
-      <p>hi</p>
+    <div className="fixed inset-0 bg-black/20 backdrop-blur-sm flex items-center justify-center z-10">
+      <form
+        method="POST"
+        className="bg-gray-50 max-w-md md:max-w-xl flex w-full flex-col items-center p-4 rounded border border-gray-400 gap-4 mx-2 xl:max-w-2xl relative"
+      >
+        <button className="absolute right-4 cursor-pointer" onClick={onClose}>
+          <XIcon />
+        </button>
+        <textarea
+          type="text"
+          className="border-b border-gray-400 w-full resize-none p-2 focus:outline-none mt-4"
+          placeholder="What’s happening?"
+          rows={10}
+        />
+        <div className="flex justify-between w-full">
+          <div className="flex gap-2">
+            <button className="cursor-pointer">
+              <img src={image} alt="import image" />
+            </button>
+            <button className="cursor-pointer">
+              <img src={clapperboard} alt="import a video" />
+            </button>
+          </div>
+          <button className="bg-black text-gray-50 px-6 py-2 rounded">
+            Post
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
