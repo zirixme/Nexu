aabbf28137aaf9548b7922aa3effdda54f7c93a3
zirixme <zirix.me@gmail.com>
2025-07-30 13:26:56 +0300

    feat: made it functional now you can you it to post text

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,9 +1,20 @@
 import image from "../assets/lucide/image.svg";
 import clapperboard from "../assets/lucide/clapperboard.svg";
-
+import { useState } from "react";
 import { XIcon } from "lucide-react";
+import { createPost } from "../api/auth.js";
 
 export const Create = ({ onClose }) => {
+  const [post, setPost] = useState("");
+  const handlePost = async () => {
+    try {
+      await createPost({ text: post });
+      setPost("");
+      onClose();
+    } catch (error) {
+      console.error("Failed creating post:", error);
+    }
+  };
   return (
     <div className="fixed inset-0 bg-black/20 backdrop-blur-sm flex items-center justify-center z-10">
       <form
@@ -18,6 +29,8 @@ export const Create = ({ onClose }) => {
           className="border-b border-gray-400 w-full resize-none p-2 focus:outline-none mt-4"
           placeholder="What’s happening?"
           rows={10}
+          onChange={(e) => setPost(e.target.value)}
+          value={post}
         />
         <div className="flex justify-between w-full">
           <div className="flex gap-2">
@@ -28,7 +41,11 @@ export const Create = ({ onClose }) => {
               <img src={clapperboard} alt="import a video" />
             </button>
           </div>
-          <button className="bg-black text-gray-50 px-6 py-2 rounded">
+          <button
+            className="bg-black text-gray-50 px-6 py-2 rounded"
+            onClick={handlePost}
+            type="button"
+          >
             Post
           </button>
         </div>
