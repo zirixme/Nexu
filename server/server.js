25e8b220f9aed756939dc01c034599392b7c611e
zirixme <zirix.me@gmail.com>
2025-08-10 13:14:00 +0300

    init: WebSocket.io on backend

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-
+import { createServer } from "http";
+import { Server } from "socket.io";
 // routes imports //
 import authRoutes from "./routes/authRoutes.js";
 import postsRoutes from "./routes/postsRoutes.js";
@@ -19,11 +20,36 @@ app.use(
     credentials: true,
   })
 );
+
+// Socket.Io
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: "http://localhost:5173" },
+});
+
+// WebSocket connection
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join_room", (roomId) => {
+    socket.join(roomId);
+    console.log(`User ${socket.id} joined room ${roomId}`);
+  });
+
+  socket.on("send_message", (data) => {
+    io.to(data.roomId).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postsRoutes);
 app.use("/api/u", usersRoutes);
 
-app.listen(process.env.PORT || 3000, () => {
+httpServer.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}.`);
 });
