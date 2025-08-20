19b3791d5a8ba61dcbd03f1401c71893aebcfbd8
zirixme <zirix.me@gmail.com>
2025-08-20 11:57:51 +0300

    fix: naming from getMessages to getMessage

--- server/routes/messagesRoutes.js
+++ server/routes/messagesRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { getMessages, sendMessages } from "../controllers/messageController.js";
+import { getMessages, sendMessage } from "../controllers/messageController.js";
 
 const router = express.Router();
 
 router.use(protect);
 
 router.get("/", getMessages);
-router.post("/", sendMessages);
+router.post("/", sendMessage);
 
 export default router;
