f2f7439834a45e0b13f2a04dc79a835c61c27eed
zirixme <zirix.me@gmail.com>
2025-08-21 14:47:15 +0300

    fix: after loggin we check for islogged in instead of accessToken

--- client/src/components/RequireAuth.jsx
+++ client/src/components/RequireAuth.jsx
@@ -2,10 +2,10 @@ import { Navigate } from "react-router";
 import { useAuth } from "./AuthContext.jsx";
 
 export const RequireAuth = ({ children }) => {
-  const { accessToken, loading } = useAuth();
+  const { isLoggedIn, loading } = useAuth();
 
   if (loading) return <div>Loading...</div>;
-  if (!accessToken) return <Navigate to="/signin" replace />;
+  if (!isLoggedIn) return <Navigate to="/signin" replace />;
 
   return children;
 };
