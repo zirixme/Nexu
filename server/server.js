66a8ff8a2fd2bde49b314f2b0ed4abcde5ca5cda
zirixme <zirix.me@gmail.com>
2025-08-21 14:01:10 +0300

    fix: made cors dynamic

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
