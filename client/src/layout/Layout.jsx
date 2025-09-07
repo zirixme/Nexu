1c2ab1833d6c00f3948615036bdfd572d7c3f010
zirixme <zirix.me@gmail.com>
2025-09-07 10:57:11 +0300

    style: safe area under home button

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -3,9 +3,9 @@ import { Header } from "../components/Header.jsx";
 
 export const Layout = () => {
   return (
-    <div className="h-screen bg-gray-50 dark:bg-gray-950 dark:text-white">
+    <div className="h-screen bg-gray-50 dark:bg-gray-950 dark:text-white safe-area">
       <Header />
-      <main className="flex justify-center md:pl-18 xl:pl-41 bg-gray-50 dark:bg-gray-950 dark:text-white">
+      <main className="flex justify-center md:pl-18 xl:pl-41 bg-gray-50 dark:bg-gray-950 dark:text-white safe-area">
         <Outlet />
       </main>
     </div>
