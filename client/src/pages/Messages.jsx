79b38ba8c0b9fe585e3dd043697c03580fc9e3b1
zirixme <zirix.me@gmail.com>
2025-09-07 11:13:34 +0300

    fix: safe area background color

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -216,7 +216,7 @@ export const Messages = () => {
                 value={text}
                 onChange={(e) => setText(e.target.value)}
                 placeholder="Type a message..."
-                className="flex-1 p-2 border border-gray-400 dark:border-gray-600 rounded outline-0"
+                className="flex-1 p-2 border border-gray-400 dark:border-gray-600 rounded outline-0 pb-[calc(env(safe-area-inset-bottom)+1rem)]"
                 onKeyDown={(e) => e.key === "Enter" && handleSend()}
               />
               <button
